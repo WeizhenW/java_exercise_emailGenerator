2a6f3872b665a3b5d1e0b8acfc2b0e5984a9c6ea
WeizhenW <wangwzhen@gmail.com>
2019-09-30 11:15:30 -0500

    generate account number

--- src/bankaccountApp/Account.java
+++ src/bankaccountApp/Account.java
@@ -7,7 +7,10 @@ public abstract class Account implements IBaseRate {
     String sSN;
 
     double accountBalance;
-    int accountNumber;
+
+    //5 digits unique sequential number
+    static int index = 10000;
+    String accountNumber;
     double interestRate;
 
     //constructor at abstract class - take in common properties
@@ -15,13 +18,27 @@ public abstract class Account implements IBaseRate {
         this.name = name;
         this.sSN = sSN;
         this.accountBalance = initialDeposit;
-        System.out.println("New account name: " + this.name + "\nSSN is " + this.sSN + "\nbalance is $" + accountBalance);
+        System.out.println("New account name: " + this.name
+                + "\nSSN is " + this.sSN
+                + "\nbalance is $" + accountBalance);
+
+        //set account number
+        index ++;
+        this.accountNumber = setAccountNumber();
+        System.out.println("account number: " + this.accountNumber);
 
     }
 
     //basic methods
-    public int setAccountNumber() {
-        return accountNumber;
+    public String setAccountNumber() {
+        //get last two digits of ssn
+        String lastTwoOfSSN = sSN.substring(sSN.length()-2);
+        //get 5 digits unique sequential number
+        int uniqueID = index;
+        //get 3 digits random number
+        //cast to int from double
+        int randomNumber = (int) (Math.random()*1000);
+        return lastTwoOfSSN + uniqueID + randomNumber;
     }
     public double deposit(int amount) {
         accountBalance += amount;
